341419a3d9684d778a68ac695d682f75af3548af
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-06-19 10:30:30 +0300

    update in my server file

--- Student_finance /server.js
+++ Student_finance /server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/budgets', budgetsRoutes);
+app.use('/api/users', userRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
